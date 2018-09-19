ef3d3273b37564a4858c23982db7246676a48624
ntueteam5 <39110121+ntueteam5@users.noreply.github.com>
2018-09-19 11:29:52 +0800

    Add files via upload

---
+++ ntue5__wk2-2/ntue5__wk2-2.ino
@@ -0,0 +1,25 @@
+int freq = 5000;
+int ledChannel = 0;
+int resolution = 8;
+const int ledPin = 36; 
+void setup() {
+ 
+  ledcSetup(ledChannel, freq, resolution);
+  ledcAttachPin(36, ledChannel);
+  pinMode(ledPin,OUTPUT);
+}
+ 
+void loop() {
+ 
+  for (int dutyCycle = 0; dutyCycle <= 255; dutyCycle++) {
+    ledcWrite(ledChannel, dutyCycle);
+    delay(7);
+  }
+ 
+  for (int dutyCycle = 255; dutyCycle >= 0; dutyCycle--) {
+    ledcWrite(ledChannel, dutyCycle);
+    delay(7);
+  }
+ 
+}
+
